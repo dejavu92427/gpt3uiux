34d151ea12a886a0c2ded2610c5e20e95e93ec1c
dejavu92427 <dejavu92427@gmail.com>
2021-12-03 22:56:16 +0800

    12031056

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
         </div>
         <Brand />
         <WhatGPT3 />
-        <Features />
         <Possibility />
         <Cta />
         <Blog />
--- src/container/features/Features.jsx
+++ src/container/features/Features.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "./features.css";
 
-function Features() {
-  return <div className="gpt3__features">features</div>;
+function Features({ title, text }) {
+  return (
+    <div className="gpt3__features">
+      <div className="gpt3__features-title">
+        <div className="gpt3__features-line"></div>
+        {title}
+      </div>
+      <div className="gpt3__feature-text">{text}</div>
+    </div>
+  );
 }
 
 export default Features;
